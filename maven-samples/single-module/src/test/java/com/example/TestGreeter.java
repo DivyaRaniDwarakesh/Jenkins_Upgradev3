93a63c7b5ada59a90b86b58c1a3b398916ca6890
Divya <divyaranikannan@gmail.com>
2023-10-03 13:24:58 +0530

    rectify

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -35,18 +35,10 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-  @Test
-  public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World; Thank you for joining";
+    @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, Thanks you making me Happy";
 
-    assertThat(greeter.greet(someone), containsString(someone));
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-
-  @Test
-  public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World; Thank you for joining";
-
-    assertThat(greeter.greet(someone), containsString(someone));
-  }
-
 }
